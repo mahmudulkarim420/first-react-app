621ab709129eae2ba0777be2d18ddbbf5f3a4b3f
Coder. Naiem <126259025+Naiem548@users.noreply.github.com>
2025-09-19 10:13:54 +0600

    first-react-commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Countries from './Components/Countries'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello World</h1>
+
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Countries.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Countries = () => {
+  return (
+    <div>
+      <h2>Country List</h2>
+      <ul>
+        <li>Country 1</li>
+        <li>Country 2</li>
+        <li>Country 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Countries;
